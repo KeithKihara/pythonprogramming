5683f6dbf64cde4adaba216790c4708be30d2d8c
KeithKihara <keytakingz@gmail.com>
2023-02-10 10:56:09 +0300

    python list

---
+++ f.py
@@ -0,0 +1,30 @@
+#my first python list
+numbers = [45,67,34,76,23,25]
+print(len(numbers))
+#index
+print(numbers.index(45))
+names = ["python","java","tanscript"] 
+#append
+# for i in range(len(numbers))
+#      print(numbers(i))
+# names.append("chris")
+print(names)
+#pop = removes an element at a given index
+print(numbers.pop(1))
+print(numbers)
+for i in range(len(numbers)):
+    print(numbers[i])
+#REMOVE - removes the first occurrence 
+zero = [0]*10
+print(zero)
+x = [22]*30
+print(x)
+#maximum & minimum value
+nums = [1,4,9,16,25,36]
+max(nums)
+even_num = []
+for num in range[20]:
+    if (num % 2==0):
+        even_nums.append(num)
+for i in even_nums
+    print(even_nums[i])
---
+++ myfirstpython list.py
@@ -0,0 +1,19 @@
+#my first python list
+numbers = [45,67,34,76,23,25]
+print(len(numbers))
+#index
+print(numbers.index(45))
+names = ["python","java","tanscript"] 
+#append
+# for i in range(len(numbers))
+#      print(numbers(i))
+# names.append("chris")
+print(names)
+#pop = removes an element at a given index
+print(numbers.pop(1))
+print(numbers)
+for i in range(len(numbers)):
+    print(numbers[i])
+#REMOVE - removes the first occurrence 
+zero = [0]*10
+print[0]
